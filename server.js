f0aa3c558326e3855bb77a8adbf66b3cb4e07b00
MrPerry365 <1987nowhere@gmail.com>
2022-06-19 22:52:52 -0400

    add port fix

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 
 
 
 app.listen(3000, () => {
-    console.log(`API server now on port 3000!`);
+    console.log(`API server now on port ${PORT}!`);
 });
