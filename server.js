8ee3313816c6fbaeac46135f8204376855b9fbdb
MrPerry365 <1987nowhere@gmail.com>
2022-06-22 18:44:05 -0400

    make index.html button link to notes.html

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const fs = require('fs');
 
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 
 
 
-app.listen(3000, () => {
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
