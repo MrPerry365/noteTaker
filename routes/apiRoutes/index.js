dbc6b39af7890a4a93e18099965c8aa526639093
MrPerry365 <1987nowhere@gmail.com>
2022-06-23 09:16:06 -0400

    add adjustments to routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const noteRoutes = require ({notes}); 
+
+router.use(noteRoutes);
+
+module.exports = router;
+
+
+
+
+
+
+// ----------------------------------------------------------------
+// fix lines 2 & 4
